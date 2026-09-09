ca38bb0dbade64bb7e05d3fc6f4afceea6f48a27
thelmamedran <thelma.medran@gmail.com>
2026-09-09 18:02:27 -0300

    first commit

---
+++ code/TP1/src/Parser.hs
@@ -0,0 +1,76 @@
+module Parser where
+
+import           Text.ParserCombinators.Parsec
+import           Text.Parsec.Token
+import           Text.Parsec.Language           ( emptyDef )
+import           AST
+
+-----------------------
+-- Función para facilitar el testing del parser.
+totParser :: Parser a -> Parser a
+totParser p = do
+  whiteSpace lis
+  t <- p
+  eof
+  return t
+
+-- Analizador de Tokens
+lis :: TokenParser u
+lis = makeTokenParser
+  (emptyDef
+    { commentStart    = "/*"
+    , commentEnd      = "*/"
+    , commentLine     = "//"
+    , opLetter        = char '='
+    , reservedNames   = ["true", "false", "skip", "if", "else", "repeat", "until"]
+    , reservedOpNames = [ "+"
+                        , "-"
+                        , "*"
+                        , "/"
+                        , "<"
+                        , ">"
+                        , "&&"
+                        , "||"
+                        , "!"
+                        , "="
+                        , "=="
+                        , "!="
+                        , ";"
+                        , ","
+                        ]
+    }
+  )
+
+-----------------------------------
+--- Parser de expresiones enteras
+-----------------------------------
+intexp :: Parser (Exp Int)
+intexp = chainl1 intterm addop
+
+addop :: Parser (Exp Int -> Exp Int -> Exp Int)
+addop = (reservedOp lis "+" >> return Plus)
+  <|> (reservedOp lis "-" >> return Minus)
+
+intterm :: Parser (Exp Int)
+intterm = undefined
+
+------------------------------------
+--- Parser de expresiones booleanas
+------------------------------------
+
+boolexp :: Parser (Exp Bool)
+boolexp = undefined
+
+-----------------------------------
+--- Parser de comandos
+-----------------------------------
+
+comm :: Parser Comm
+comm = undefined
+
+
+------------------------------------
+-- Función de parseo
+------------------------------------
+parseComm :: SourceName -> String -> Either ParseError Comm
+parseComm = parse (totParser comm)
